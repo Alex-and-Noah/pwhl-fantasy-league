4c41f7930e5d83cac002171550ed8c8623fce095
Noah Bolohan <noah.bolohan@gmail.com>
2025-11-10 10:17:58 -0500

    Implementation of correct scoring scheme (close #6)

--- R/get_roster_points_from_game.R
+++ R/get_roster_points_from_game.R
@@ -6,34 +6,77 @@ library(dplyr)
 #'
 #' @param team_rosters data.frame of Fantasy team rosters
 #' @param player_box_for_game Player box stats for a given game
+#' @param schedule Season schedule
 #' @return data.frame of points earned by each fantasy team
 #' @import dplyr
 #' @export
 
 get_roster_points_from_game <- function(
   team_rosters,
-  player_box_for_game
+  player_box_for_game,
+  schedule
 ) {
   game_points <- list()
+
+  game_from_schedule <- schedule |>
+    filter(
+      game_id == player_box_for_game$skaters$game_id[[1]]
+    )
 
   for (team_name in names(team_rosters)) {
     game_points[[team_name]] <- player_box_for_game$skaters |>
       filter(
         player_id %in% team_rosters[team_name][[1]]$player_id
       ) |>
-      select(
-        points
+      mutate(
+        sum = rowSums(
+          across(
+            c(
+              goals,
+              assists
+            )
+          )
+        )
       ) |>
-      sum() +
-      player_box_for_game$goalies |>
-        filter(
-          player_id %in% team_rosters[team_name][[1]]$player_id
-        ) |>
+      select(
+        sum
+      ) |>
+      sum()
+
+    game_points[[team_name]] <- game_points[[team_name]] +
+      2 *
+        team_rosters[team_name][[1]] |>
+          filter(
+            team ==
+              game_from_schedule |>
+                select(
+                  winner
+                ) |>
+                pull()
+          ) |>
+          nrow()
+
+    if (
+      game_from_schedule |>
         select(
-          points
+          winner
         ) |>
-        sum() |>
-        as.numeric()
+        pull() ==
+        "Final OT"
+    ) {
+      if (game_from_schedule$home_team == game_from_schedule$winner) {
+        loser <- game_from_schedule$away_team
+      } else {
+        loser <- game_from_schedule$home_team
+      }
+
+      game_points[[team_name]] <- game_points[[team_name]] +
+        team_rosters[team_name][[1]] |>
+          filter(
+            team == loser
+          ) |>
+          nrow()
+    }
   }
 
   return(game_points)
--- R/get_roster_points_per_game.R
+++ R/get_roster_points_per_game.R
@@ -52,7 +52,8 @@ get_roster_points_per_game <- function(
       player_boxes_per_game |>
         map(
           get_roster_points_from_game,
-          team_rosters = team_rosters
+          team_rosters = team_rosters,
+          schedule = schedule
         ) |>
         bind_rows() |>
         mutate(
