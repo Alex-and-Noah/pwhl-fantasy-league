a800a6ef4513ce76569cec5a27a030162d0b4cf9
Noah Bolohan <noah.bolohan@gmail.com>
2026-08-12 13:24:51 -0400

    Fix to fantasy roster points overall computation

--- R/compute_fantasy_roster_points_overall.R
+++ R/compute_fantasy_roster_points_overall.R
@@ -21,6 +21,11 @@ compute_fantasy_roster_points_overall <- function(
       fantasy_team_roster$skaters
     ) > 0
   ) {
+    fantasy_team_roster$skaters <- fantasy_team_roster$skaters |>
+      mutate(
+        fantasy_points = 0
+      )
+
     fantasy_points_per_skater <- lapply(
       player_boxes_per_game,
       function(x) {
@@ -76,13 +81,9 @@ compute_fantasy_roster_points_overall <- function(
         )
 
       fantasy_team_roster$skaters <- fantasy_team_roster$skaters |>
-        merge(
-          fantasy_points_per_skater
-        )
-    } else {
-      fantasy_team_roster$skaters <- fantasy_team_roster$skaters |>
-        mutate(
-          fantasy_points = 0
+        rows_update(
+          fantasy_points_per_skater,
+          by = "name"
         )
     }
 
@@ -99,6 +100,11 @@ compute_fantasy_roster_points_overall <- function(
       fantasy_team_roster$goalies
     ) > 0
   ) {
+    fantasy_team_roster$goalies <- fantasy_team_roster$goalies |>
+      mutate(
+        fantasy_points = 0
+      )
+
     fantasy_points_per_goalie <- lapply(
       player_boxes_per_game,
       function(x) {
@@ -153,13 +159,9 @@ compute_fantasy_roster_points_overall <- function(
         )
 
       fantasy_team_roster$goalies <- fantasy_team_roster$goalies |>
-        merge(
-          fantasy_points_per_goalie
-        )
-    } else {
-      fantasy_team_roster$goalies <- fantasy_team_roster$goalies |>
-        mutate(
-          fantasy_points = 0
+        rows_update(
+          fantasy_points_per_goalie,
+          by = "name"
         )
     }
 
